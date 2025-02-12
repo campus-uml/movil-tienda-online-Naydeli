d742e8438ebce9cf807d48119dfc6f3062647c45
naydeli <est.nolivasm@uml.edu.ni>
2025-02-11 22:35:36 -0600

    ajuste de contenedor principal a pantallas moviles correccion

--- src/components/RecipeCard.tsx
+++ src/components/RecipeCard.tsx
@@ -32,7 +32,7 @@ const RecipeCard: React.FC = () => {
   }, [currentIndex]);
 
   return (
-    <div className="flex flex-col items-center justify-between  bg-gray-50 px-2 lg:px-0 min-h-[95vh] max-w-4xl mx-auto w-full">
+    <div className="flex flex-col items-center justify-between  bg-gray-50 px-2 lg:px-0 min-h-[80vh] max-w-4xl mx-auto w-full">
       {/* Barra de progreso tipo "Estados de WhatsApp" */}
       <div className="flex gap-1 w-full max-w-3xl px-2">
         {slides.map((_, index) => (
